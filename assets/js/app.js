6f9a20640ebeafb51ff780ca1d550e9463b312df
MMB-Laptop <mmboudot@gmail.com>
2023-09-29 20:01:13 +0200

    :sparkles: Javascript functions to open/close the modal and to get the form's data in it

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,55 @@
-
-// on objet qui contient des fonctions
 var app = {
+  // Modal
+  modal: document.getElementById("addListModal"),
 
-  // fonction d'initialisation, lancée au chargement de la page
+  // Init Function
   init: function () {
-    console.log('app.init !');
-  }
+    app.listeners();
+  },
 
+  // Events listener Function
+  listeners: function () {
+    const newCardButton = document.getElementById("addListButton");
+    const closeModalButtons = document.querySelectorAll(".close");
+    const validFormButton = document.querySelector(".input");
+
+    newCardButton.addEventListener("click", app.showModal);
+
+    for (let i = 0; i < closeModalButtons.length; i++) {
+      closeModalButtons[i].addEventListener("click", app.hideModal);
+    }
+
+    validFormButton.addEventListener("submit", app.hundleFormSubmit);
+  },
+
+  // Show modal Function
+  showModal: function () {
+    app.modal.classList.add("is-active");
+  },
+
+  // Hide modal Function
+  hideModal: function () {
+    app.modal.classList.remove("is-active");
+  },
+
+  // Hundle form submission Function
+  hundleFormSubmit: function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    for (let data of formData) {
+      let name = data[0];
+      let value = data[1];
+
+      formData.append(name, value);
+    }
+
+    app.makeDomCard(formData);
+  },
+
+  // Make a card in the DOM Function
+  makeDomCard: function () {},
 };
 
-
-// on accroche un écouteur d'évènement sur le document : quand le chargement est terminé, on lance app.init
-document.addEventListener('DOMContentLoaded', app.init );
+document.addEventListener("DOMContentLoaded", app.init);
