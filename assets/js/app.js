eb80772509f4919f1d4135583bd761130bd97036
Hodor DESKTOP <mmboudot@gmail.com>
2023-10-04 00:31:38 +0200

    :poop: Modal as module but CORS error

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var app = {
+const app = {
   base_url: "http://localhost:3000/boards/1/cards",
   errorBox: document.querySelector(".error-box"),
 
--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,22 +1,91 @@
 export const modal = {
-  // Card Modal Initialization
-  makeCard: function (e) {
-    const nameEn = "card";
-    const nameFr = "carte";
-    const isCard = true;
-    app.createModal(e, nameEn, nameFr, isCard);
+  base_url: "http://localhost:3000/boards/1/cards",
+
+  // Modal Strings
+  modalConstants: {
+    cardNameEn: "card",
+    sufixCardFr: "carte",
+    todoNameEn: "todo",
+    sufixTodoFr: "tâche",
+    inputSufix: "Input",
   },
 
-  // Todo Modal Initialization
-  makeTodo: function (e) {
-    const nameEn = "todo";
-    const nameFr = "tâche";
-    const isCard = false;
-    app.createModal(e, nameEn, nameFr, isCard);
+  // To get the first letter of the string uppercased
+  capzFirstLetter: function (value) {
+    const upperIt = value.charAt(0).toUpperCase() + value.slice(1);
+    return upperIt;
+  },
+
+  // New Modal Initialization
+  newModalAttributes: function (e, isCard) {
+    const data = {
+      isCard,
+      method: "add",
+      formValue: "",
+      inputValue: "",
+      idValue: "",
+      titleValue: "Ajouter une ",
+      placeholderValue: "Ma nouvelle ",
+      submitValue: "Créer",
+    };
+
+    modal.fillModalWithAttributes(e, data);
+  },
+
+  // Edit Modal Initialization
+  editModalAttributes: function (e, isCard, cardId, todoId) {
+    console.log(isCard);
+
+    const data = {
+      isCard,
+      method: "edit",
+      formValue: "",
+      inputValue: "",
+      idValue: "",
+      titleValue: "Editer une ",
+      submitValue: "Editer",
+      placeholderValue: "edit",
+      cardTodoIds: {
+        cardId,
+        todoId,
+      },
+    };
+
+    console.log(data);
+
+    modal.fillModalWithAttributes(e, data);
+  },
+
+  // Modals Data Definition
+  fillModalWithAttributes: function (e, data) {
+    if (data.isCard === true) {
+      data.titleValue += modal.modalConstants.sufixCardFr;
+      data.placeholderValue += modal.modalConstants.sufixCardFr;
+      data.formValue =
+        modal.modalConstants.cardNameEn + modal.capzFirstLetter(data.method);
+      data.inputValue = data.method + modal.modalConstants.inputSufix;
+      data.idValue =
+        data.method +
+        modal.capzFirstLetter(modal.modalConstants.cardNameEn) +
+        "Modal";
+    } else {
+      data.titleValue += modal.modalConstants.sufixTodoFr;
+      data.placeholderValue += modal.modalConstants.sufixTodoFr;
+      data.formValue =
+        modal.modalConstants.todoNameEn + modal.capzFirstLetter(data.method);
+      data.inputValue = data.method + modal.modalConstants.inputSufix;
+      data.idValue =
+        data.method +
+        modal.capzFirstLetter(modal.modalConstants.todoNameEn) +
+        "Modal";
+      console.log("hello");
+    }
+
+    modal.createModal(e, data);
   },
 
   // Creating and adding the wanted modal to the DOM
-  createModal: function (e, nameEn, nameFr, isCard) {
+  createModal: function (e, data) {
     const template = document.getElementById("modal");
     const section = document.querySelector(".section");
 
@@ -29,27 +98,20 @@ export const modal = {
     const label = clone.querySelector(".label");
     const input = clone.querySelector(".input");
     const modal = clone.querySelector(".modal");
-
-    // Clone Values definiton
-    const modalIdValue = isCard
-      ? app.modalConstants.cardModalIdValue
-      : app.modalConstants.cardTodoIdValue;
-    const titleValue = `${app.modalConstants.titlePrefix}${nameFr}`;
-    const formIdValue = `${nameEn}${app.modalConstants.formSufix}`;
-    const inputIdValue = `${nameEn}${app.modalConstants.inputSufix}`;
-    const placeholderValue = `${app.modalConstants.placeholderPrefix}${nameFr}`;
+    const submit = clone.querySelector(".is-success");
 
     // Clone Values setting
-    modal.setAttribute("id", modalIdValue);
-    form.setAttribute("id", formIdValue);
-    title.textContent = titleValue;
-    label.setAttribute("for", inputIdValue);
-    input.setAttribute("id", inputIdValue);
-    input.setAttribute("placeholder", placeholderValue);
+    modal.setAttribute("id", data.idValue);
+    form.setAttribute("id", data.formValue);
+    title.textContent = data.titleValue;
+    label.setAttribute("for", data.inputValue);
+    input.setAttribute("id", data.inputValue);
+    input.setAttribute("placeholder", data.placeholderValue);
     input.value = input.placeholder;
+    submit.textContent = data.submitValue;
 
     // Getting cardId with data-card-id's value of HTML .panel element
-    if (!isCard) {
+    if (!data.isCard) {
       const idInput = clone.getElementById("todoId");
       const cardId = e.target.closest(".panel").dataset.cardId;
       idInput.value = cardId;
@@ -62,12 +124,97 @@ export const modal = {
     section.parentNode.insertBefore(clone, section.nextSibling);
 
     // Events listeners
-    form.addEventListener("submit", (e) =>
-      app.handleFormModalSubmission(e, isCard)
+    const { isCard } = data;
+    const { cardTodoIds } = data;
+
+    console.log(cardTodoIds);
+
+    form.addEventListener("submit", (ev) =>
+      modal.handleFormModalSubmission(ev, isCard, cardTodoIds)
     );
-    const closeModalButtons = document.querySelectorAll(".close");
+    const closeModalButtons = form.querySelectorAll(".close");
     closeModalButtons.forEach((button) => {
-      button.addEventListener("click", app.hideModal);
+      button.addEventListener("click", modal.hideModal);
+    });
+  },
+
+  // Getting the new card form's data on submission
+  handleFormModalSubmission: async function (e, isCard, cardTodoIds) {
+    e.preventDefault();
+
+    try {
+      const form = e.target;
+      let options = {};
+      const formData = new FormData(form);
+      const title = formData.get("title");
+
+      if (!title) {
+        alert(
+          `Impossible de publier une ${isCard ? "carte" : "todo"} sans nom !`
+        );
+        return;
+      }
+
+      if (!cardTodoIds) {
+        options = {
+          method: "POST",
+          body: formData,
+        };
+
+        if (isCard === true) {
+          const response = await fetch(modal.base_url, options);
+          if (!response.ok) throw json;
+          const json = await response.json();
+        } else {
+          const cardId = formData.get("id");
+          const response = await fetch(
+            `${modal.base_url}/${cardId}/todos`,
+            options
+          );
+
+          if (!response.ok) throw json;
+
+          const json = await response.json();
+        }
+      } else {
+        formData.delete("id");
+        options = {
+          method: "PATCH",
+          body: formData,
+        };
+
+        if (isCard === true) {
+          const { cardId } = cardTodoIds;
+
+          const response = await fetch(`${modal.base_url}/${cardId}`, options);
+          if (!response.ok) throw json;
+          const json = await response.json();
+        } else {
+          const { cardId } = cardTodoIds;
+          const { todoId } = cardTodoIds;
+
+          const response = await fetch(
+            `${modal.base_url}/${cardId}/todos/${todoId}`,
+            options
+          );
+          if (!response.ok) throw json;
+          const json = await response.json();
+        }
+      }
+
+      location.reload();
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  },
+
+  // Hide all modals
+  hideModal: function () {
+    const modals = document.querySelectorAll(".modal");
+
+    modals.forEach((modal) => {
+      modal.classList.remove("is-active");
     });
   },
 };
